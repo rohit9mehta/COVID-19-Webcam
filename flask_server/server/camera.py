3c52762a68ad7ad174705995cfba11bb7f6d43b9
Rohit Mehta <rohit9mehta@gmail.com>
2020-06-21 13:22:12 -0700

    fixed frame rate, consistency

--- flask_server/server/camera.py
+++ flask_server/server/camera.py
@@ -1,5 +1,7 @@
 import cv2
 import sys
+import imutils
+
 # replace with path to folder
 sys.path.append('/Users/Rohit/Desktop/Corona')
 from faceDetection import detector
@@ -35,31 +37,33 @@ class VideoCamera(object):
 
     def get_frame(self):
         frame = detector(self.video)
+        frame = imutils.resize(frame, width=400)
         success, image = self.video.read()
         image = cv2.resize(frame, None, fx=ds_factor, fy=ds_factor,
         interpolation=cv2.INTER_AREA)                    
         gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
         face_rects = faceCascade.detectMultiScale(gray, 1.3, 5)
-        hand_rects = handCascade.detectMultiScale(gray, 1.3, 5)
-        fist_rects = handCascadeFist2.detectMultiScale(gray, 1.3, 5)
+        # hand_rects = handCascade.detectMultiScale(gray, 1.3, 5)
+        # fist_rects = handCascadeFist2.detectMultiScale(gray, 1.3, 5)
         for (x,y,w,h) in face_rects:
             cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
             break
-        for (x, y, w, h) in hand_rects:
-            x2 = x
-            y2 = y
-            x3 = x+w
-            y3 = y+h
-            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            break
+        # for (x, y, w, h) in hand_rects:
+        #     x2 = x
+        #     y2 = y
+        #     x3 = x+w
+        #     y3 = y+h
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        #     break
 
-        for (x, y, w, h) in fist_rects:
-            x4 = x
-            y4 = y
-            x5 = x+w
-            y5 = y+h
-            cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
-            break
+        # for (x, y, w, h) in fist_rects:
+        #     x4 = x
+        #     y4 = y
+        #     x5 = x+w
+        #     y5 = y+h
+        #     cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        #     break
         # encode OpenCV raw frame to jpg and displaying it
         ret, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
